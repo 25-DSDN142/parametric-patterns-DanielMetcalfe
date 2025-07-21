25141b2d730627187c2493369994b4859460347b
DanielMetcalfe <metcaldani@myvuw.ac.nz>
2025-07-22 11:56:07 +1200

    Cros stich effect test
    adding rasterisation function to turn whatever is drawn on a pgraphics object into grid of ellipses, rectangles etc. to emulate cross stich dots. using an ellipse to test/
    
    rasterising pgraphics objects learnt through Tim Roedenbroker pGraphics processing course. rewritten in p5.js by me from one of my previous processing projects.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,136 +1,67 @@
-let green;
-let pink;
-let navy;
-let fg3;
-let flowerSize =0.5;
-let flowerAmount=1;
-let pinkBAmount = 115; ///255 for yellow;
-let flowerX = -20;
-let flowerY =20;
- let flowerCW=100;
- let petalWidth=90;
- let petalHeight=100;
-
-let navyRAmount= 40;  //40 navy
-
-
-
+// let pixelX = 200;
+// let pixelY = 200;
+let gridsX = 50;
+let gridsY = 50;
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(DEVELOP_GLYPH);
-  //pWallpaper.output_mode(GRID_WALLPAPER);
-  
+  pWallpaper.output_mode(GRID_WALLPAPER);
   pWallpaper.resolution(NINE_LANDSCAPE);
-  pWallpaper.show_guide(false); //set this to false when you're ready to print
+  pWallpaper.show_guide(false);
 
-  //Grid settings
   pWallpaper.grid_settings.cell_width  = 200;
   pWallpaper.grid_settings.cell_height = 200;
-  pWallpaper.grid_settings.row_offset  =60;
-
-green = color(255,255,0);
-  pink = color(255,pinkBAmount,122);
-  navy = color(navyRAmount,115,255);
-  orange= color(255,140,82);
- 
+  pWallpaper.grid_settings.row_offset  = 0;
 
 
 }
 
 function wallpaper_background() {
-  let bgColour= green;
-  background(bgColour); //light honeydew green colour
+  background(240, 255, 240);
 }
 
-function my_symbol() { // do not rename this function. Treat this similarly to a Draw function
-  // clear();
-  // background(green);
-  translate(-flowerX,flowerY);
-  push();
-translate(-20,0);
-  for(let x = 0; x < flowerAmount; x ++){
-    scale(flowerSize);
-let flowerTiles= 200/flowerAmount;
-let flowerSpace= x*flowerTiles;
- translate(flowerSpace,0);
-
-
-    flower();
-  }
+function my_symbol() {
+  let pattern= createGraphics(200, 200);
   
-  pop();
+  pattern.stroke(0);
+  pattern.fill(255, 0, 0);
+  pattern.circle(100, 100, 50);
+
+ 
+  // image(pattern, 0, 0);
+
+  crossstitch(pattern); 
 }
 
+function crossstitch(pattern) {
 
-function flower () {
+  //rasterising pgraphics objects learnt through Tim Roedenbroker pGraphics processing course. rewritten in p5.js by me from one of my previous processing projects. 
 
- 
+  let wallpaperTileW= 200;
+  let wallpaperTileH= 200;
+  let pixelW = wallpaperTileW / gridsX;
+  let pixelH = wallpaperTileH/ gridsY;
 
+  let patternClr = pattern.get();  
 
-  // translate(flowerXpos,80);
-
-  let crclSize = 35;
-  let crclX = 200/2;
-  let crclY = 200/4;
-
-
-
-
-
-
-//petals 
-   
-  let petlSize = 30;
-  let xOff = petlSize ;
-  let yOff = xOff;
-
-  let xOff2 = petlSize * (2/3);
-     let yOff2 = petlSize * (2/3);
- 
-  push();
-  
-   noStroke();
-  
-   let petalColour= navy;
-
-   fill(petalColour);
-  
-   
-
- ellipse(crclX-xOff,crclY,petalWidth, petalHeight
- );
-   ellipse(crclX+xOff,crclY,petalWidth, petalHeight);
- 
-   ellipse(crclX,crclY-yOff, petalWidth,petalHeight);
-   ellipse(crclX,crclY+yOff,petalWidth, petalHeight);
-
-   ellipse(crclX-xOff2,crclY-yOff2,petalWidth, petalHeight);
-   ellipse(crclX+xOff2,crclY-yOff2, petalWidth,petalHeight);
-
-   ellipse(crclX-xOff2,crclY+yOff2,petalWidth, petalHeight);
-   ellipse(crclX+xOff2,crclY+yOff2, petalWidth,petalHeight);
-
- 
-   pop();
-
-// centre;
-  push();
-  
   noStroke();
-  fill (orange);
-  ellipse(crclX, crclY,flowerCW ,flowerCW); 
 
-  pop();
+  for (let x = 0; x < gridsX; x++) {
+    for (let y = 0; y < gridsY; y++) {
+      let px = int(x * pixelW + pixelW / 2);
+      let py = int(y * pixelH + pixelH / 2);
 
+      let clr = patternClr.get(px, py);
+      let bright = brightness(clr);
+      let size = map(bright, 0, 255, 0, 1);
 
+      let maskW = pixelW * size;
+      let maskH = pixelH * size;
 
-
-
-
-
-
+      push();
+      fill(clr);
+      translate(px, py);
+      ellipse(0, 0, maskW, maskH);
+      pop();
+    }
+  }
 }
-
-
-
-
