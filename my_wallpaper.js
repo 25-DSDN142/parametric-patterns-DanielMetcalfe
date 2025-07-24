43e287ca195a54efa3c78bc8e6ebe424e42e8cea
DanielMetcalfe <metcaldani@myvuw.ac.nz>
2025-07-25 00:25:42 +1200

    Testing adjustable radial distribution of objects and if statements
    In order to create the cross stitch/patch like symmetrical patterns I will incorporate radial symmetry/distribution.
    
    Here I am testing adjustable radial distribution of objects using simpler objects to figure out the mechanics. Allows for even spacing around a circle based on the amount of objects. Learnt through p5.js cos and sine example, and used my knowledge of for loops to make the code simpler.
    
    https://p5js.org/examples/angles-and-motion-sine-cosine/
    
    also exploring how if statements can be used to adjust colours. In this case using every even x amount of the forloop as a different colour to the odd.
    
    I also further simplified the cross stich function to make it work better.

--- my_wallpaper.js
+++ my_wallpaper.js
@@ -1,12 +1,21 @@
 
+//cross stich settings
 let gridsX = 50;
 let gridsY = 50;
-let circleSize=100;
-let circleX=200;
-let circleY=200;
+
+///test radial paths for adjustable amounts of objects
+let objAmount = 12; ///amount of objects
+let radius=60; //circlular path radius
+let centerX=100; ///centre point of circular path
+let centerY=100;
+let objectSize = 20; //size of test object
+
+let orange, blueD, blueL;
+
+
 
 function setup_wallpaper(pWallpaper) {
-  pWallpaper.output_mode(GRID_WALLPAPER);
+  pWallpaper.output_mode(DEVELOP_GLYPH);
   pWallpaper.resolution(NINE_LANDSCAPE);
   pWallpaper.show_guide(false);
 
@@ -18,37 +27,76 @@ function setup_wallpaper(pWallpaper) {
 }
 
 function wallpaper_background() {
-  background(240, 255, 240);
+  background(0);
 }
 
 function my_symbol() {
+
   let pattern = rawPattern();
- 
   // image(pattern, 0, 0);  //test raw pattern
   crossstitch(pattern);
 }
 
 function rawPattern() {
   let pg = createGraphics(200, 200);
- 
- //corner tiling test
-  pg.stroke(0);
-  pg.fill(255, 0, 0);
-  pg.circle(circleX, circleY, circleSize);
-  pg.circle(circleX-200, circleY, circleSize);
-  pg.rectMode(CENTER);
-  pg.ellipse(circleX, circleY-200, circleSize);
-  pg.ellipse(circleX-200, circleY-200, circleSize);
- 
+  pg.angleMode(DEGREES); 
+  let orange = color(241,87,85);
+  let blueD = color (32,59,114);
+  let blueL = color(69,128,194);
 
+
+  pg.background(orange);
+  pg.fill(blueD);
+  pg.stroke(0);
+
+  //centre circle
+  pg.ellipse(centerX, centerY, 50);
   
+  //corner circles
+  push();
+  
+  let cornerD= 100;
+  let cornerSize= 100;
+
+  pg.ellipse(centerX-cornerD, centerY-cornerD, cornerSize);
+  pg.ellipse(centerX+cornerD, centerY-cornerD, cornerSize);
+  pg.ellipse(centerX-cornerD, centerY+cornerD,cornerSize);
+  pg.ellipse(centerX+cornerD, centerY+cornerD,cornerSize);
+  pop();
+
+
+  ////objects on the circular path- using cosine and sine to postion objects.
+  for (let x = 0; x < objAmount; x++) {
+    let angle = x * (360 / objAmount) - 90;
+
+    if (x % 2 == 0){
+
+    pg.fill(blueD);
+    }
+
+    else{
+      pg.fill(blueL);
+    }
+
+    let ellipseX = centerX + radius * pg.cos(angle);
+    let ellipseY = centerY + radius * pg.sin(angle);
+
+    pg.ellipse(ellipseX, ellipseY, objectSize, objectSize);
+
+
+ 
+  }
+  
+
   return pg;
 }
 
 function crossstitch(pattern) {
   let tileW = 200 / gridsX;
   let tileH = 200 / gridsY;
-  let patternClr = pattern.get(); 
+  let crossW= tileW;
+  let crossH= tileH;
+  let patternClr = pattern.get();
 
   noStroke();
 
@@ -58,16 +106,11 @@ function crossstitch(pattern) {
       let py = int(y * tileH + tileH / 2);
 
       let clr = patternClr.get(px, py);
-      let bright = brightness(clr);
-      let size = map(bright, 0, 255, 0, 1);
-
-      let w = tileW * size;
-      let h = tileH * size;
+      fill(clr);
 
       push();
-      fill(clr);
       translate(px, py);
-      ellipse(0, 0, w, h);
+      ellipse(0, 0, crossW, crossH);
       pop();
     }
   }
